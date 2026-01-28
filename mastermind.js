f8af0a3a7b1ba8e6555988f14066824317a26cda
holly.harrold <hollyharrold12@gmail.com>
2026-01-28 13:10:02 +0000

    Created Simple Layout

--- mastermind.js
+++ mastermind.js
@@ -13,9 +13,39 @@ function createBoard() {
         }
 
         var board = document.getElementById("colourBoard")
+        // create marking for row
+        var markContainer = document.createElement("div");
+        for (var n = 0; n < 4; n++) {
+            if (n == 0) {
+                markContainer.id = "markContainer" + n;
+                markContainer.classList.add("markContainer");
+                row.appendChild(markContainer)
+            }
+            var mark = document.createElement("div");
+            mark.classList.add("mark");
+            mark.id = "mark" + n;
+            markContainer.appendChild(mark)
+        }
         board.appendChild(row);
+    }
+
+    
+
+}
+
+// guess the computers colours
+function playComputer() {
+
+    // let computer select four random colours
+    var colourArray = ["red", "blue", "yellow", "green", "purple", "pink"];
+    var computerColourChoices = [];
+
+    for (var i = 0; i < 4; i++) {
+        var randomColour = Math.floor(Math.random() * coloursArray.length);
     }
 
 }
 
 createBoard();
+
+document.getElementById("playComputerButton").addEventListener("click", playComputer);
